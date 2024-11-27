c66792c06677e883adb93bbe7d815b36de001304
Arabath <axelhobrecht@hotmail.com>
2024-11-27 11:42:24 -0300

    'TaskForm.tsx' added

--- taskfront/src/App.tsx
+++ taskfront/src/App.tsx
@@ -5,7 +5,7 @@ import TaskList from "./components/TaskList";
 function App() {
   return (
     <div className="h-screen bg-zinc-900 text-white flex justify-center items-center">
-      <div className="bg-gray-950 p-4 w-2/5">
+      <div className="bg-gray-950 p-4 w-1/5">
         <h1 className="text-3xl font-bold text-center block my-2">Task App</h1>
         <TaskForm/>
         <TaskList/>
