ad8882f1aebedc823f5ff53ed35449ea25ed1dbb
Arabath <axelhobrecht@hotmail.com>
2024-11-28 08:41:22 -0300

    context added

--- taskfront/src/api/task.ts
+++ taskfront/src/api/task.ts
@@ -1,16 +1,31 @@
-const API = 'http://localhost:3000'
+import { CreateTask, UpdateTask } from "../interface/task.interface";
 
-interface Task {
-    title: string;
-    description?: string;
-    done?: boolean
-}
+const API = "http://localhost:3000";
 
-export const createTaskRequest = (task: Task) =>
-    fetch(`${API}/Tasks`, {
-        method: 'POST',
-        body: JSON.stringify(task),
-        headers: {
-            'Content-Type':'application/json'
-        }
-    })
+export const getTasksRequest = async () => fetch(`${API}/tasks`);
+
+export const createTaskRequest = (task: CreateTask) =>
+  fetch(`${API}/tasks`, {
+    method: "POST",
+    body: JSON.stringify(task),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+export const deleteTaskRequest = (id: string) =>
+  fetch(`${API}/tasks/${id}`, {
+    method: "Delete",
+  });
+
+export const updateTaskRequest = async (id: string, task: UpdateTask) =>
+  fetch(`${API}/tasks/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(task),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+
+export const getTaskRequest = async (id: string) => fetch(`${API}/tasks/${id}`);
